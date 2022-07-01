a8d5158f243fcc82a81c350c216a80e377b4b2bb
Dylan King <xylvnking@gmail.com>
2022-07-01 17:34:41 -0400

    gatsby starter

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/dylanking6132/xylvnking/markdown-editor/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/dylanking6132/xylvnking/markdown-editor/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/dylanking6132/xylvnking/markdown-editor/src/pages/index.js"))
+}
+
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `markdown-editor`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
