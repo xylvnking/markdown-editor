77c7f93ddad1feb1af543bc853d679ca9d6e49eb
Dylan King <xylvnking@gmail.com>
2022-07-10 13:47:29 -0400

    added comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,62 +23,48 @@ const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 const users = collection(db, "users")
 
+// i think i can use this to create data for the user so they have some entries by default
 // functions.auth.user().onCreate((user) => {
 // })
 
 const IndexPage = () => {
   
-  const [isAuthorized, setIsAuthorized] = React.useState()
+  // const [isAuthorized, setIsAuthorized] = React.useState()
   const [userData, setUserData] = React.useState()
   // const [users, setUsers] = React.useState()
   
-  const getAllDocumentsInCollection = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    // console.log(`users is: ` + JSON.stringify(querySnapshot, null, 2))
-    
-    querySnapshot.forEach((doc) => {
-      
-      // console.log(doc.id, " => ", doc.data());
-      
-  });
-  }
   const getDocumentData = async () => {
+    // if the user is signed in
     if (userData) {
       // console.log(`users isfsdfsdf: ` + JSON.stringify(userData.uid, null, 2))
+      // get a reference to their document which is named after their UID
       const docRef = doc(db, "users", userData.uid);
+      // make sure that reference has been set
       const docSnap = await getDoc(docRef);
+      // if the reference is set properly
       if (docSnap.exists()) {
+        // log all of their data to the console
         console.log("Document data:", docSnap.data());
       } else {
-        // doc.data() will be undefined in this case
         console.log("No such document!");
       }
     }
   }
 
   onAuthStateChanged(auth, (user) => {
+    // if the user is signed in, 
+    // set user data stores information about the user in state
+    // get document data gets all of their stored data 
+        //which are field:values held in one document named according their uid
     if (user) {
-      // getCollections()
-      // setIsAuthorized(true)
       setUserData(user)
-      // getAllDocumentsInCollection()
       getDocumentData()
-
-      
-      // console.log(user.uid)
-      
-      // console.log(userData)
-
-
-      const uid = user.uid;
-      // userData = user 
-      
+    // otherwise if the user is not signed in, set user data to none
+    // and probably set document data to the "unauthorized data" like before
     } else {
-      // setIsAuthorized(false)
       setUserData()
     }
   })
-  
   
   const signInWithGoogle = async () => {
     await signInWithPopup(auth, provider).then((result) => {
@@ -91,15 +77,12 @@ const IndexPage = () => {
       console.log("signing out")
     })
   }
-
-  console.log("rerender")
   
   return (
     <main className="app">
       {/* {`authorization status: ${(isAuthorized ? `signed in` : `logged out`)}`} */}
       {userData ? <button onClick={signUserOut}>Sign Out</button> : <button onClick={signInWithGoogle}>Sign In with Google</button>}
       {userData ? <AuthorizedEditorComponent userData={userData}/> : ""}
-      
     </main>
     )
 }
